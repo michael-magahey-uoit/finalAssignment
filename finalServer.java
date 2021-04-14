2a32c159b5ad2aaf94b107e15bcc11edd9e75640
unknown <keith.middleton@ontariotechu.net>
2021-04-13 23:46:42 -0400

    Last Commit

--- finalServer.java
+++ finalServer.java
@@ -3,6 +3,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.paint.Color;
 import javafx.scene.Group;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
@@ -13,6 +14,7 @@ import javafx.application.Platform;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import javafx.scene.layout.VBox;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
@@ -26,22 +28,108 @@ import java.util.Vector;
 
 public class finalServer extends Application
 {
-    Canvas canvas;
-    Scene orderbookScene, priceScene, mainScene, loginScene;
+    Canvas orderCanvas, pricesCanvas;
+    Scene orderbookScene, pricesScene, mainScene, loginScene;
     GraphicsContext orderbookGraphics, priceGraphics;
     Socket connection;
     finalServerThread priceOrderHandler;
     static ServerSocket serverSocket = null;
     static finalServerThread thread = null;
+    static float[] bidHistory = new float[200];
+    static float[] askHistory = new float[200];
+    static int historyIndex = 0;
 
-    public void paintOrderbook()
+    public void paintOrderbook(float[] price, float[] volume, String[] orderType)
     {
+        int lastX = 0;
 
+        float yMax = Float.MAX_VALUE;
+        float yMin = Float.MIN_VALUE;
+
+        for (int i = 0; i < volume.length; i++)
+        {
+            yMax = Float.max(yMax, volume[i]);
+            yMin = Float.min(yMin, volume[i]);
+        }
+
+        pricePositions = price.length;
+        if (pricePositions != volume.length)
+        {
+            System.out.println("Error! Prices dont match volume count.");
+            return;
+        }
+
+        int xBase = (int)(800 / price.length);
+        xBase -= (price.length * 10);
+
+        for (int i = 0; i < price.length; i++)
+        {
+            if (orderType[i] == "BOOK_TYPE_BUY")
+            {
+                orderbookGraphics.setStroke(Color.BLUE);
+            }
+            else if (orderType[i] == "BOOK_TYPE_SELL")
+            {
+                orderbookGraphics.setStroke(Color.RED);
+            }
+            int height = (int)(volume[i] / yMax) * 700;
+            orderbookGraphics.fillRect(lastX, 750, xBase, height);
+            lastX = lastX + xBase + 10;
+        }
+        
     }
 
-    public void paintPrices()
+    public void paintPrices(float bid, float ask)
     {
 
+        for (int i = historyIndex; i > 0; i--)
+        { 
+            array[i] = array[i-1]; 
+        }
+
+        bidHistory[0] = bid;
+        askHistory[0] = ask;
+
+        historyIndex++;
+
+        float yMax = Float.MIN_VALUE;
+        float yMin = Float.MAX_VAULE;
+
+        for (int i = 0; i < historyIndex; i++)
+        {
+            yMax = Float.max(yMax, bidHistory[i]);
+            yMax = Float.max(yMax, askHistory[i]);
+
+            yMin = Float.min(yMin, bidHistory[i]);
+            yMin = Float.min(yMin, askHistory[i]);
+        }
+
+        priceGraphics.setStroke(Color.BLUE);
+        int prevX = 50;
+        float norm = (bidHistory[0] - yMin) / (yMax - yMin);
+        float prevY = 500 - (norm * 400);
+        float incX = 450 / bidHistory.length;
+        for (int i = 1; i < historyIndex; i++)
+        {
+            float num = (bidHistory[i] - yMin) / (yMax - yMin);
+            float yPos = 500 - (num * 400);
+            priceGraphics.strokeLine(prevX, prevY, (int)(prevX + incX), (int)yPos);
+            prevX = prevX + (int)incX;
+            prevY = (int)yPos;
+        }
+        priceGrahics.setStroke(Color.RED);
+        int prevX = 50;
+        float norm = (askHistory[0] - yMin) / (yMax - yMin);
+        float prevY = 500 - (norm * 400);
+        float incX = 450 / askHistory.length;
+        for (int i = 1; i < historyIndex; i++)
+        {
+            float num = (askHistory[i] - yMin) / (yMax - yMin);
+            float yPos = 500 - (num * 400);
+            priceGraphics.strokeLine(prevX, prevY, (int)(prevX + incX), (int)yPos);
+            prevX = prevX + (int)incX;
+            prevY = (int)yPos;
+        }
     }
 
     @Override
@@ -50,6 +138,66 @@ public class finalServer extends Application
         primaryStage.setTitle("Depth of Market reader - Idle...");
 
         //Make price and orderbook forms and set context to globals
+        //Return Button
+        Button orderReturn = new Button("Back to Main");
+        Button priceReturn = new Button("Back to Main");
+
+        //Order Book
+        VBox orderLayout = new VBox(40); 
+        orderbookScene = new Scene(orderLayout, 1000, 800);
+        primaryStage.setTitle("Order Book");
+
+        Group orderRoot = new Group();
+        orderCanvas = new Canvas();
+
+        orderCanvas.widthProperty().bind(primaryStage.widthProperty());
+        orderCanvas.heightProperty().bind(primaryStage.heightProperty());
+        orderRoot.getChildren().add(orderCanvas);
+
+        primaryStage.setScene(orderbookScene);
+
+        orderLayout.getChildren().add(orderReturn);
+        orderLayout.getChildren().add(orderCanvas);
+
+        orderbookGraphics = orderCanvas.getGraphicsContext2D();
+
+        //Prices
+        VBox priceLayout = new VBox(40); 
+        pricesScene = new Scene(priceLayout, 1000, 800);
+        primaryStage.setTitle("Prices");
+
+        Group priceRoot = new Group();
+        pricesCanvas = new Canvas();
+
+        pricesCanvas.widthProperty().bind(primaryStage.widthProperty());
+        pricesCanvas.heightProperty().bind(primaryStage.heightProperty());
+        priceRoot.getChildren().add(pricesCanvas);
+
+        primaryStage.setScene(pricesScene);
+
+        priceLayout.getChildren().add(priceReturn);
+        priceLayout.getChildren().add(pricesCanvas);
+
+
+        priceGraphics = pricesCanvas.getGraphicsContext2D();
+
+        //Return Button
+
+        orderReturn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                primaryStage.setScene(mainScene);
+                System.out.println("Clicked on Back to Main");
+            }
+        });
+
+        priceReturn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                primaryStage.setScene(mainScene);
+                System.out.println("Clicked on Back to Main");
+            }
+        });
 
         GridPane myGrid = new GridPane();
         myGrid.setAlignment(Pos.CENTER);
@@ -117,7 +265,7 @@ public class finalServer extends Application
             public void handle(ActionEvent actionEvent)
             {
                 primaryStage.setTitle("Final Project - Price");
-                primaryStage.setScene(priceScene);
+                primaryStage.setScene(pricesScene);
             }
         });
 
--- finalServerThread.java
+++ finalServerThread.java
@@ -7,11 +7,13 @@ public class finalServerThread extends Thread
     protected Socket socket     = null;
     protected PrintWriter out   = null;
     protected BufferedReader in = null;
+    protected finalServer callback = null;
 
-    public finalServerThread(Socket socket)
+    public finalServerThread(Socket socket, finalServer callback)
     {
         super();
         this.socket = socket;
+        this.callback = callback;
         try
         {
             out = new PrintWriter(socket.getOutputStream(), true);
@@ -33,6 +35,7 @@ public class finalServerThread extends Thread
         try 
         {
             socket.close();
+            System.out.println("[DEBUG] - Socket Closed! Session Ended.");
         }
         catch (IOException ex)
         {
@@ -46,33 +49,36 @@ public class finalServerThread extends Thread
         try
         {
             message = in.readLine();
-            //System.out.println(message);
-            if(message != null){
-                String[] packet = message.split("[|]");
+            String[] packetBacklog = message.split("[ ]");
+            for (String backlogMessage : packetBacklog)
+            {
+                System.out.println("[backlog]: " + backlogMessage);
+                String[] packet = backlogMessage.split("[|]");
                 String symbol = packet[0];
                 String type = packet[1];
-                System.out.println(packet[0]);
-                System.out.println(packet[1]);
-                System.out.println(packet[2]);
-                if (type == "Tick")
+                if (type.equals("Tick"))
                 {
                     float bid = Float.parseFloat(packet[2]);
                     float ask = Float.parseFloat(packet[3]);
                     System.out.println("New Price! Bid: " + bid + " | Ask: " + ask);
-                    //Update price scene
+                    callback.paintPrices(bid, ask);
                 }
-                else if (type == "OrderBook")
+                else if (type.equals("OrderBook"))
                 {
-                    String[] orderbook = packet[2].split("!");
+                    String[] orderbook = packet[2].split("[!]");
+                    float[] prices = new float[orderbook.length];
+                    float[] volumes = new float[orderbook.length];
+                    String[] orderTypes = new String[orderbook.length];
+                    int counter = 0;
                     for (String pricePoint : orderbook)
                     {
-                        String[] data = pricePoint.split("-");
-                        float price = Float.parseFloat(data[0]);
-                        float volume = Float.parseFloat(data[1]);
-                        String bookType = data[2];
+                        String[] data = pricePoint.split("[-]");
+                        prices[counter] = Float.parseFloat(data[0]);
+                        volumes[counter] = Float.parseFloat(data[1]);
+                        orderTypes[counter] = data[2];
+                        counter++;
                     }
-                    System.out.println("[DEBUG] - Orderbook: " + packet[2]);
-                    //Update orderbook scene
+                    callback.paintOrderbook(prices, volumes, orderTypes);
                 }
                 else{
                     System.out.println(type);
